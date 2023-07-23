de725560ab484401d49aaf455730fca81c82cf1f
jlew92 <jlew92@gmail.com>
2023-07-22 22:56:58 -0500

    Updated token

--- WordleDiscord.py
+++ WordleDiscord.py
@@ -7,41 +7,41 @@ import time
 import threading
 import responses
 
+
 class DiscordGameBot:
     currentGameDict = {}
     mainwindow = None
-    
+
     def __init__(self) -> None:
-        
-        TOKEN = "MTEyNzM0Mjk4MzYyNTMyNjYxMg.GOeSrt.H32CNUngp4nQRGJjobweODx2Vcb9h39ePrvM4A"
+
+        TOKEN = "MTEyNzM0Mjk4MzYyNTMyNjYxMg.GfWMk1."
+        token += "1jHNWvWdnw_l-AvVA9gYh8DwvlKGNKWL0N5ghQ"
         client = discord.Client()
+
         @client.event
         async def onReady():
             print(f'{client.user} is now running')
-            
+
         client.run(TOKEN)
-        #TEMP create c
-        #TEMP create current
+        # TEMP create c
+        # TEMP create current
         # refreshThread = threading.Thread(target=self.captureScreenShot)
         # refreshThread.daemon = True
         # refreshThread.start()
 
-        
-        
     def setMainWindow(self, mw):
         self.mainwindow = mw
-        
+
     def captureScreenShot(self):
         time.sleep(2)
         screen = QtWidgets.QApplication.primaryScreen()
         screenShot = screen.grabWindow(self.mainwindow.winId())
         screenShot.save("bullBird.jpg", 'jpg')
-        
-        
+
     async def receiveMessage(self):
         pass
-    
-    
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mw = MainWindow()
