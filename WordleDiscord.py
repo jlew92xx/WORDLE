fe60b1629fb4963ab0e89a349a5e0a96a5dc4227
Jonathan Lewis <JonathanmLewis092@gmail.com>
2023-07-22 22:52:43 -0500

    token trouble

--- WordleDiscord.py
+++ WordleDiscord.py
@@ -5,15 +5,26 @@ from PyQt5 import QtWidgets
 from MainWindow import MainWindow
 import time
 import threading
+import responses
+
 class DiscordGameBot:
     currentGameDict = {}
     mainwindow = None
     
     def __init__(self) -> None:
+        
+        TOKEN = "MTEyNzM0Mjk4MzYyNTMyNjYxMg.GOeSrt.H32CNUngp4nQRGJjobweODx2Vcb9h39ePrvM4A"
+        client = discord.Client()
+        @client.event
+        async def onReady():
+            print(f'{client.user} is now running')
+            
+        client.run(TOKEN)
+        #TEMP create c
         #TEMP create current
-        refreshThread = threading.Thread(target=self.captureScreenShot)
-        refreshThread.daemon = True
-        refreshThread.start()
+        # refreshThread = threading.Thread(target=self.captureScreenShot)
+        # refreshThread.daemon = True
+        # refreshThread.start()
 
         
         
@@ -25,6 +36,12 @@ class DiscordGameBot:
         screen = QtWidgets.QApplication.primaryScreen()
         screenShot = screen.grabWindow(self.mainwindow.winId())
         screenShot.save("bullBird.jpg", 'jpg')
+        
+        
+    async def receiveMessage(self):
+        pass
+    
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mw = MainWindow()
